a41697bc8aa7b9c2e3957db57e9a44bcf60440c0
Leonardo Thibes <leonardothibes@yahoo.com.br>
2012-12-25 17:27:09 -0200

    update

--- compiler/compile.sh
+++ compiler/compile.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-if [ "$1" == "" ]; then
-	echo "Um arquivo de entrada deve ser informado."
-	exit 1
-fi
+#if [ "$1" == "" ]; then
+#	echo "Um arquivo de entrada deve ser informado."
+#	exit 1
+#fi
 
-PRG=$(echo $1 | sed 's/\.m//g')
-gcc -o $PRG $PRG.m -framework Foundation
+#PRG=$(echo $1 | sed 's/\.m//g')
+#gcc -o $PRG $PRG.m -framework Foundation
+gcc -o $1 *.m -framework Foundation
