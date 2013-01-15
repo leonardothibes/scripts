3a6f4796689ee2ed71819617aac46d8f7af8bd0b
Leonardo Thibes <leonardothibes@yahoo.com.br>
2013-01-15 15:57:00 -0200

    update

--- post/pagseguro.php
+++ post/pagseguro.php
@@ -1,7 +1,16 @@
+#!/usr/bin/php
 <?php
 
-require_once 'Zend/Http/Request.php';
+require_once 'Zend/Http/Client.php';
 
-$url = 'http://telesena.desenv.local/compra/confirmacao';
+$url    = 'http://telesena.desenv.local/pagseguro/request';
+$client = new Zend_Http_Client($url);
+$result = $client->setParameterPost('transid', rand(1000,5000))
+	             ->request(Zend_Http_Client::POST);
 
+if($result->isError()) {
+	echo sprintf('[%d] Error: %s', $result->getStatus(), $result->getMessage());
+}
 
+$rs = $result->getBody();
+print_r($rs);
