68d7fab51847e72f6ec1d45c15d5154dd17df345
Leonardo Thibes <eu@leonardothibes.com>
2013-12-05 17:35:46 -0200

    update

--- puppet-module-maker/puppet-module-maker.sh
+++ puppet-module-maker/puppet-module-maker.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# REFERENCE:
+#	http://docs.puppetlabs.com/puppet/2.7/reference/modules_fundamentals.html#example
+#	https://github.com/leonardothibes/puppet-memcached
+
 # Author
 WHO=`whoami`
 read -p "Input the author[${WHO}]: " AUTHOR
