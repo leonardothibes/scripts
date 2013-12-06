4fc696683745a4b8f7b329f5d9b8f8022b50a84f
Leonardo Thibes <eu@leonardothibes.com>
2013-12-06 11:43:52 -0200

    update

--- puppet-module-maker/puppet-module-maker.sh
+++ puppet-module-maker/puppet-module-maker.sh
@@ -1,13 +1,58 @@
 #!/bin/bash
 
-# REFERENCE:
-#	http://docs.puppetlabs.com/puppet/2.7/reference/modules_fundamentals.html#example
-#	https://github.com/leonardothibes/puppet-memcached
+# Modulename
+read -p "Module Name: " NAME
+if [ -z "$NAME" ]; then
+	echo "Error: Input the module name!"
+	exit 1
+fi
+# Modulename
+
+# Module Directory
+if [ -d ./${NAME} ]; then
+	echo "ERROR: Directory \"${NAME}\" exists!"
+	exit 1
+fi
+# Module Directory
+
+# Description
+read -p "Module Description: " DESCRIPTION
+if [ -z "$DESCRIPTION" ]; then
+	echo "Error: Input the module description!"
+	exit 1
+fi
+# Description
 
 # Author
 WHO=`whoami`
-read -p "Input the author[${WHO}]: " AUTHOR
-[ -z ${AUTHOR} ] && AUTHOR=${WHO}
+read -p "Author[${WHO}]: " AUTHOR
+[ -z $AUTHOR ] && AUTHOR=${WHO}
 # Author
 
+# Creating skeleton of module
+[ -d ./${NAME} ]           || mkdir ./${NAME}
+[ -d ./${NAME}/files ]     || mkdir ./${NAME}/files
+[ -d ./${NAME}/lib ]       || mkdir ./${NAME}/lib
+[ -d ./${NAME}/templates ] || mkdir ./${NAME}/templates
+[ -d ./${NAME}/spec ]      || mkdir ./${NAME}/spec
 
+[ -d ./${NAME}/tests ] || mkdir ./${NAME}/tests
+echo "include ${NAME}" > ./${NAME}/tests/init.pp
+
+[ -d ./${NAME}/manifests ] || mkdir ./${NAME}/manifests
+echo "class ${NAME} {
+
+}" > ./${NAME}/manifests/init.pp
+# Creating skeleton of module
+
+# Modulefile
+FILE="./${NAME}/Modulefile"
+echo "name '${AUTHOR}-${NAME}'"               > $FILE
+echo "version '0.0.0'"                       >> $FILE
+echo "author '${AUTHOR}'"                    >> $FILE
+echo "license 'Apache License, Version 2.0'" >> $FILE
+echo "summary 'UNKNOWN'"                     >> $FILE
+echo "description '${DESCRIPTION}'"          >> $FILE
+echo "source 'git://github.com/${AUTHOR}/puppet-${NAME}.git'"        >> $FILE
+echo "project_page 'http://documentup.com/${AUTHOR}/puppet-${NAME}'" >> $FILE
+# Modulefile
