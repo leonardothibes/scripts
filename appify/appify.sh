ed572ef441293a8578da2a9b9f9e70b0d7ab1a7f
Leonardo Thibes <eu@leonardothibes.com>
2013-12-28 00:44:25 -0200

    update

--- appify/appify.sh
+++ appify/appify.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-APPNAME=${2:-$(basename "$1"".sh")}
-DIR="$APPNAME.app/Contents/MacOS"
-
-if [ -a "$APPNAME.app" ]; then
-	echo "$PWD/$APPNAME.app already exists :("
+if [ -z $1 ]; then
+	echo 'App name could not be blank'
 	exit 1
 fi
 
-mkdir -p "$DIR"
-cp "$1" "$DIR/$APPNAME"
-chmod +x "$DIR/$APPNAME"
+NAME=$(echo ${1} | cut -d. -f1)
+FILE=${NAME}.sh
+DIR="$NAME.app/Contents/MacOS"
 
-echo "$PWD/$APPNAME.app"
+if [ -d "${NAME}.app" ]; then
+	echo "${NAME}.app already exists"
+	exit 1
+fi
+
+mkdir -p $DIR
+cp $FILE "${DIR}/${NAME}"
+chmod +x "${DIR}/${NAME}"
