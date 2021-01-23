37fea8b88ca039c2c22737230c615b8661fe1e8e
Leonardo Thibes <leonardothibes@gmail.com>
2021-01-22 21:19:32 -0300

    github migration tool

---
+++ git/gh-migrate.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+HERE=$(dirname $0)
+REPO=$(cat .git/config | grep url | cut -d: -f2 | cut -d. -f1)
+FROM="git@bitbucket.org:${REPO}"
+TO="git@github.com:${REPO}"
+
+clear
+echo "Github Migration Tool"
+echo ""
+echo "Migrating from ${FROM} ${TO}"
+echo ""
+
+git remote rename origin bitbucket
+git remote add origin ${TO}
+
+for BRANCH in $(git branch)
+do
+    if [ ! -d ${BRANCH} ] && [ ! -f ${BRANCH} ]; then
+        git push origin ${BRANCH}
+    fi
+done
+
+for TAG in $(git tag)
+do
+    git push origin ${TAG}
+done
+
+read -p "Remove old origin? (y/N) " REMOVE
+if [ "${REMOVE}" == "y" ] || [ "${REMOVE}" == "Y" ]; then
+    git remote rm bitbucket
+fi
+
+echo ""
+echo "Done!"
+echo ""
